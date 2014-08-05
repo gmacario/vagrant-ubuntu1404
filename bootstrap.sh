11089bcc3e563d8b5c070651e7a8ae53e278d6d9
Gianpaolo Macario <gmacario@gmail.com>
2014-08-05 15:53:57 +0200

    bootstrap.h: Some cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Quick fix to enable vbguest on VirtualBox 4.3.10
-# See https://github.com/mitchellh/vagrant/issues/3341
-if [ ! -e /usr/lib/VBoxGuestAdditions ]; then
-    sudo ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions \
-	/usr/lib/VBoxGuestAdditions || true
-fi
-
 sudo apt-get update
 
 # NOTE: If kernel is upgraded, you need to upgrade VirtualBox Guest Additions as well
@@ -20,17 +13,12 @@ sudo apt-get update
 # Upgrading from Ubuntu server to desktop
 #
 # See http://askubuntu.com/questions/322122/switching-from-server-to-desktop
-#sudo apt-get -y install ubuntu-desktop 
+#sudo apt-get -y install ubuntu-desktop
 
 # Install git and related tools
 sudo apt-get -y install git git-svn tig
 
 # Other things I will never live without...
 sudo apt-get -y install mc
-
-# Install Docker
-#sudo apt-get -y install docker.io
-
-# TODO
 
 # === EOF ===
