677fa6df39f8a1f3f156ed67934a99eb10944592
Gianpaolo Macario <gmacario@gmail.com>
2014-09-14 13:24:55 +0200

    Add comments and check for base directory
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- get_vagrant_patches.sh
+++ get_vagrant_patches.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-VAGRANT_HOME=/opt/vagrant/embedded/gems/gems/vagrant-1.6.5
+# Poor-man version control system
+# This shell script prints a diff of all files which have
+# a corresponding "*.ORIG" in the same directory
+# This may be useful when applying workarounds to installed programs
+
+#VAGRANT_HOME=/opt/vagrant/embedded/gems/gems/vagrant-1.6.5
+VAGRANT_HOME=/Applications/Vagrant/embedded/gems/gems/vagrant-1.6.3
+
+if [ ! -d "${VAGRANT_HOME}" ]; then
+    echo "ERROR: Please check VAGRANT_HOME"
+    exit 1
+fi
 
 echo "# VAGRANT_HOME=${VAGRANT_HOME}"
 
