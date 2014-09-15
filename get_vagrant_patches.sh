e016b68adec30fd70e1550ada5e56935aadad7be
Gianpaolo Macario <gmacario@gmail.com>
2014-09-15 19:42:51 +0200

    Adjust VAGRANT_HOME for Cygwin
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- get_vagrant_patches.sh
+++ get_vagrant_patches.sh
@@ -6,10 +6,11 @@
 # This may be useful when applying workarounds to installed programs
 
 #VAGRANT_HOME=/opt/vagrant/embedded/gems/gems/vagrant-1.6.5
-VAGRANT_HOME=/Applications/Vagrant/embedded/gems/gems/vagrant-1.6.3
+#VAGRANT_HOME=/Applications/Vagrant/embedded/gems/gems/vagrant-1.6.3
+VAGRANT_HOME=/cygdrive/c/HashiCorp/Vagrant/embedded/gems/gems/vagrant-1.6.5
 
-if [ ! -d "${VAGRANT_HOME}" ]; then
-    echo "ERROR: Please check VAGRANT_HOME"
+if [ -z "${VAGRANT_HOME}" -o ! -d "${VAGRANT_HOME}" ]; then
+    echo "ERROR: Please check VAGRANT_HOME: ${VAGRANT_HOME}"
     exit 1
 fi
 
