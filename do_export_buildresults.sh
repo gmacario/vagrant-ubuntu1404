b57440fb909e6ee442719fcea1723bd3a6ba8c76
Gianpaolo Macario <gmacario@gmail.com>
2014-10-13 13:00:02 +0200

    Add support for remote DESTBASEDIR
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- do_export_buildresults.sh
+++ do_export_buildresults.sh
@@ -9,10 +9,19 @@ if [ "$BUILDDIR" = "" ]; then
     exit 1
 fi
 
-DESTDIR=$(echo $BUILDDIR | sed -e "s|$HOME|/vagrant/tmp|")
+#DESTBASEDIR="/vagrant/tmp"
+DESTBASEDIR="gmacario@kruk.solarma.it:~/tmp"
+
+DESTDIR=$(echo $BUILDDIR | sed -e "s|$HOME|$DESTBASEDIR|")
 echo "DBG: DESTDIR=$DESTDIR"
 
-mkdir -p "$DESTDIR/tmp/deploy"
-cd "$BUILDDIR/tmp/deploy" && rsync -avz . "$DESTDIR/tmp/deploy/"
+if [ $(echo $DESTDIR | grep ":") ]; then
+    REMOTEHOST=$(echo $DESTDIR | sed -e 's/:.*$//')
+    REMOTEDIR=$(echo $DESTDIR | sed -e 's/^.*://')
+    ssh "${REMOTEHOST}" mkdir -p "${REMOTEDIR}/tmp/deploy"
+else
+    mkdir -p "${DESTDIR}/tmp/deploy"
+fi
+cd "${BUILDDIR}/tmp/deploy" && rsync -avz . "${DESTDIR}/tmp/deploy/"
 
 # EOF
