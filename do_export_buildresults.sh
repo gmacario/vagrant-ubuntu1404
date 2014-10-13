e2f5fbcd8e79e83624bd262ea40b1fc8f530c37f
Gianpaolo Macario <gmacario@gmail.com>
2014-10-13 12:11:37 +0200

    Add script for exporting build results
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

---
+++ do_export_buildresults.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#set -x
+set -e
+
+echo "DBG: BUILDDIR=$BUILDDIR"
+if [ "$BUILDDIR" = "" ]; then
+    echo "ERROR: This script should be executed from within the guest OS"
+    exit 1
+fi
+
+DESTDIR=$(echo $BUILDDIR | sed -e "s/\/home\/vagrant/\/vagrant\/tmp/")
+echo "DBG: DESTDIR=$DESTDIR"
+
+mkdir -p "$DESTDIR/tmp/deploy"
+cd "$BUILDDIR/tmp/deploy" && rsync -avz . "$DESTDIR/tmp/deploy/"
+
+# EOF
