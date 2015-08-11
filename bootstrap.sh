1d0cd3fe091cc480483b8b410bba4a39901793b0
Gianpaolo Macario <gmacario@gmail.com>
2015-08-11 16:51:06 +0200

    Install packages required by Yocto/OE

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,7 +52,7 @@ sudo apt-get -y install bc lzop make ncurses-dev u-boot-tools
 #    libdbus-glib-1-2:i386 libasound2:i386
 
 # Install packages required by MEL 2014.05 (Yocto)
-#sudo apt-get -y install gawk g++ diffstat texinfo chrpath
+sudo apt-get -y install gawk g++ diffstat texinfo chrpath
 
 # Install packages required by Vista 3.9.0
 #sudo apt-get -y install xterm
